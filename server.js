37736b26c7ca014af0ede132b722420963fce73d
vinaykanth97 <vinaykennedy@gmail.com>
2024-11-29 22:54:00 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,74 @@
+import { createServer } from 'http'
+import { GetProductData, GetBodyData, WriteDataToFile, Logger } from './utils.js'
+
+
+const PORT = process.env.PORT
+
+
+
+const server = createServer((req, res) => {
+
+    Logger(req, res, async () => {
+        const requiredHeaders = {
+            'Content-Type': 'application/json'
+        }
+        res.writeHead(200, requiredHeaders)
+        let productsDatas = await GetProductData()
+
+        if (req.url === "/getproductdetails" && req.method === "GET") {
+            res.write(JSON.stringify(productsDatas))
+            res.end()
+        }
+        else if (req.url === "/createproduct" && req.method === "POST") {
+            let getBodyJson = await GetBodyData(req, res)
+            if (!getBodyJson) {
+                res.write(JSON.stringify({ message: 'Sufficient Datas not sent' }))
+                res.end()
+            } else {
+                productData.push({ id: crypto.randomUUID(), ...getBodyJson })
+                res.statusCode = 201
+                WriteDataToFile(productData)
+                res.write(JSON.stringify({ message: 'Product Created Successfully' }))
+                res.end()
+            }
+        } else if (req.url.match(/\/updateproductdetails\/([0-9A-Fa-f]{8}(-[0-9A-Fa-f]{4}){3}-[0-9A-Fa-f]{12}$)/) && req.method === "PUT") {
+            let getBodyJson = await GetBodyData(req, res)
+            getBodyJson = JSON.parse(getBodyJson)
+            let getSpecificId = req.url.split('/')[2]
+            let findSpecificIndex = productsDatas.findIndex(products => products.id === getSpecificId)
+            if (findSpecificIndex !== -1) {
+                productsDatas[findSpecificIndex] = {
+                    id: getSpecificId,
+                    ...getBodyJson
+                }
+                WriteDataToFile(productsDatas)
+                res.write(JSON.stringify({ message: "Product updated Successfully" }))
+            } else {
+                res.write(JSON.stringify({ message: "No Such product found" }))
+            }
+            res.end()
+        } else if (req.url.match(/\/removeproduct\/([0-9A-Fa-f]{8}(-[0-9A-Fa-f]{4}){3}-[0-9A-Fa-f]{12}$)/) && req.method === "DELETE") {
+            let getSpecificId = req.url.split('/')[2]
+            let findSpecificIndex = productsDatas.findIndex(products => products.id === getSpecificId)
+            if (findSpecificIndex !== -1) {
+                let filterProducts = productsDatas.filter(products => products.id !== getSpecificId)
+                WriteDataToFile(filterProducts)
+                res.write(JSON.stringify({ message: "Product deleted Successfully" }))
+            } else {
+                res.write(JSON.stringify({ message: "No Such product found" }))
+            }
+
+            res.end()
+        } else {
+            res.write(JSON.stringify({ message: "Route Not Found" }))
+        }
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`Server listening in PORT:${PORT}`);
+})
+
+server.on('error', (e) => {
+    console.log(e);
+});
---
+++ utils.js
@@ -0,0 +1,46 @@
+import path from 'path';
+import productData from './data/products.json' with { type: "json" }
+import { fileURLToPath } from 'url';
+import { writeFileSync } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const GetProductData = () => {
+    return new Promise((resolve, reject) => {
+        try {
+            resolve(productData)
+        } catch (error) {
+            console.log(error);
+            reject(productData)
+        }
+    })
+}
+
+export const WriteDataToFile = (data) => {
+    writeFileSync(path.resolve(__dirname, 'data/products.json'), JSON.stringify(data))
+}
+
+export const GetBodyData = async (req, res) => {
+    return new Promise((resolve, reject) => {
+        try {
+            let bodyData = ""
+            req.on('data', (chunk) => {
+                bodyData += chunk.toString()
+            })
+            req.on('end', () => {
+                resolve(bodyData)
+            })
+            req.on('error', (err) => {
+                reject(err);
+            })
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+export const Logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next()
+}
